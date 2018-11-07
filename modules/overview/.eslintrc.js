d6af3f0d0693b8bda9087d648cdb57e2916783e8
Danny <ngo.danny11@gmail.com>
2018-11-07 11:22:10 -0800

    Added overview module

---
+++ modules/overview/.eslintrc.js
@@ -0,0 +1,15 @@
+
+/**
+ * These rules enforce the Hack Reactor Style Guide
+ *
+ * Visit this repo for more information:
+ *   https://github.com/reactorcore/eslint-config-hackreactor
+ */
+
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": "airbnb",
+  "globals": {
+    "window": true,
+  }
+};
