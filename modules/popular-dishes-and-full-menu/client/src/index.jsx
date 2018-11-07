31b1bc7ee5c254ad6a2d346a2c083158f448f25a
Danny <ngo.danny11@gmail.com>
2018-11-07 14:54:59 -0800

    deleted popular dishes module from overview branch

--- modules/popular-dishes-and-full-menu/client/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-
-// if what gets passed in is a restaurant id, can easily pass that down and just update the state with the restaurant name
-ReactDOM.render(<App restaurantName='nulla' />, document.getElementById('PopularDishes'));
